4297f42c47124e198c1d022b973508e842131add
kotarono0519 <kotaro.yamaguchi@quantum-g.jp>
2023-07-05 11:54:41 +0900

    delete

--- api/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const db = require('../../dbConnection');
-const kakeiboController = require('../controllers/kakeiboController');
-
-router.get('/kekibo/get', (req, res) => kakeiboController.getData(req, res, db));
-router.post('/kakeibo/post',(req, res) => kakeiboController.postData(req, res, db));
-router.put('/kekibo/put',(req, res) => kakeiboController.putData(req, res, db));
-
-module.exports = router;
